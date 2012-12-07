ec35f53fb8ccbb3305ceb099e96cf3a6edb8c50e
TheInventionist <Gregan@TheInventionist.com>
2012-12-07 08:16:16 -0500

    Updated HttpParametersUtils to have the json creator

--- gdx/src/com/badlogic/gdx/HttpParametersUtils.java
+++ gdx/src/com/badlogic/gdx/HttpParametersUtils.java
@@ -1,12 +1,15 @@
 
 package com.badlogic.gdx;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Map;
 import java.util.Set;
 
 import com.badlogic.gdx.Net.HttpRequest;
+import com.badlogic.gdx.utils.JsonWriter;
 
 /** Provides utility methods to work with the {@link HttpRequest} content and parameters. */
 public class HttpParametersUtils {
@@ -36,6 +39,51 @@ public class HttpParametersUtils {
 			return URLEncoder.encode(content, encoding);
 		} catch (UnsupportedEncodingException e) {
 			throw new IllegalArgumentException(e);
+		}
+	}
+
+	/** Takes a Map and converts it into json, which can be sent over http using header Content-Type = application/json
+	 * @param parameters Map<String, ?> to output in json format
+	 * @return String representing the parameters with json */
+	public static String convertJsonHttpParameters (Map<String, ?> parameters) {
+		StringWriter jsonText = new StringWriter();
+		JsonWriter writer = new JsonWriter(jsonText);
+
+		try {
+			createJson(parameters, "", writer);
+		} catch (IOException e) {
+			return "";
+		}
+		return jsonText.toString();
+
+	}
+
+	/** Run this to fill writer with JSON based on the content */
+	private static void createJson (Object content, String name, JsonWriter writer) throws IOException {
+		if (content instanceof Map) {
+			if (name == "")
+				writer.object();
+			else
+				writer.object(name);
+			Set<String> keySet = ((Map<String, ?>)content).keySet();
+			for (String key : keySet) {
+				createJson(((Map)content).get(key), key, writer);
+			}
+			writer.pop();
+		} else if (content instanceof Object[]) {
+			if (name == "")
+				writer.array();
+			else
+				writer.array(name);
+			for (Object key : (Object[])content) {
+				createJson(key, "", writer);
+			}
+			writer.pop();
+		} else {
+			if (name == "")
+				writer.value(content);
+			else
+				writer.set(name, content);
 		}
 	}
 }
